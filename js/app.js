2c8525f0227bc25d513dc482e12ffc589f1e9623
Deepak Prabhudesai <deepak@ajency.in>
2014-08-20 19:37:03 +0530

    www folder from cordova project

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// Ionic Starter App
+angular.module('starter', ['ionic', 'starter.login'])
+
+
+.run(function($ionicPlatform, $state) {
+
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+    //Goto Login
+    $state.go('login');
+
+  });
+})
+
+
+.config(function($stateProvider, $urlRouterProvider) {
+  
+  $stateProvider
+    
+    .state('login', {
+      url: "/login",
+      templateUrl: 'templates/loginView.html',
+      controller: 'LoginController'
+    })
+
+});
---
+++ js/controllers/login.js
@@ -0,0 +1,5 @@
+angular.module('starter.login', [])
+
+.controller('LoginController', function($scope) {
+	
+})
