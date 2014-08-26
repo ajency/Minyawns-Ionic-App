c036507455439948fb9f9c8730d8e23ab1dafa19
Deepak Prabhudesai <deepak@ajency.in>
2014-08-26 16:11:27 +0530

    toggle left menu

--- www/app/home/home.js
+++ www/app/home/home.js
@@ -1,5 +1,11 @@
 angular.module('starter.home', [])
 
-.controller('HomeController', ['$scope',function($scope) {
+.controller('HomeController', ['$scope', '$ionicSideMenuDelegate'
+	, function($scope, $ionicSideMenuDelegate) {
+
+		$scope.toggleLeft = function(){
+
+			$ionicSideMenuDelegate.toggleLeft();
+		};
 	
 }])
