6b122f3c0fe7c8c09e7b70ca3b8fa2700d97e456
Deepak Prabhudesai <deepak@ajency.in>
2015-06-04 16:31:35 +0530

    no message

---
+++ minyawns assets/com.parse.cordova.core.pushplugin/src/android/ParseApplication.java
@@ -0,0 +1,32 @@
+package org.apache.cordova.core;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+import com.ajency.in.minyawns.CordovaApp;
+
+public class ParseApplication extends Application 
+{
+	private static ParseApplication instance = new ParseApplication();
+
+	public ParseApplication() {
+		instance = this;
+	}
+
+	public static Context getContext() {
+		return instance;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// register device for parse
+		Parse.initialize(this, "WkWfnsbpAF555UQLYfL71yYxlGRSrB8IYfnKTx8M", "dIP7JKPlYjIBVWiCDYmRldveKnKHPDBBPevahc4M");
+		PushService.setDefaultPushCallback(this, CordovaApp.class);
+		ParseInstallation.getCurrentInstallation().saveInBackground();
+	}
+}
